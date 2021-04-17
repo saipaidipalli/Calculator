18ced89a288153e3a6576bee72e3c51ad3dbf64b
Sai Ram <saipaidipalli@gmail.com>
2021-04-17 18:43:44 +0530

    Version1

--- src/Calc.kt
+++ src/Calc.kt
@@ -7,11 +7,16 @@ fun div(x:Double , y:Double ):Double  = x/y
 fun per(x:Double , y:Double ):Double  = (x/100)*y
 
 fun main(args: Array<String>) {
-
-    var result:Double = -1000.0  // Dummy value to display the output if one only number is  displayed
+// Dummy value to display the output if one only number is  displayed
+    var result:Double = -1000.0
+// Initially keep the operator blank
     var operator = ' '
     val reader = Scanner(System.`in`)
    println("Please select '=' operator to get result")
+/*
+Looping to get input number and operator based on users choice
+and exit the loop when '=' operator is chose in order to display result
+*/
     do {
         print("Enter number : ")
         val num = reader.nextDouble()
@@ -56,11 +61,13 @@ fun main(args: Array<String>) {
             print("Enter an operator (+, -, *, /, %, =): ")
              operator = reader.next()[0]
         }
+// When only one number is chose, and no second number chosen then display the first number
        if (result == -1000.0) {
            result = num
         }
 
         }while(operator != '=')
+// To print the result
          print("$result")
     }
 
